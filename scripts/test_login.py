c0f7276b4b4890e0dea33e1c3e4db9fc01a90067
lijuanfish <1026921878@qq.com>
2019-07-11 13:41:55 +0800

    first commit

---
+++ scripts/test_login.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+class TestLogin:
+
+    def test_log1(self):
+        print("1")
+    def test_log2(self):
+        print("2")
+    def test_log3(self):
+        print("3")
+    # @pytest.mark.run(order=1)
+    def test_log4(self):
+        print("4")
+
+if __name__ == '__main__':
+    pytest.main(["-s","test_login.py"])
